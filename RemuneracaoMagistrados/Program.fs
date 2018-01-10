03f50435269a414ee0c099d73120419f4bdff814
Victor Hugo Marques <victorhm@outlook.com>
2018-01-10 10:53:35 -0200

    saving data as csv

--- RemuneracaoMagistrados/Program.fs
+++ RemuneracaoMagistrados/Program.fs
@@ -2,33 +2,8 @@
 open System.IO
 open System.Text
 open FSharp.Data
-open FSharp.Collections.ParallelSeq
 open NPOI.SS.UserModel
-open NPOI.HSSF.UserModel  
-open Newtonsoft.Json
-
-type JsonItem = { 
-    CPF : string;
-    Nome : string;
-    Cargo : string;
-    Lotacao : string;
-    Subsidio : string;
-    DireitosPessoais : string;
-    Indenizacoes : string;
-    DireitosEventuais : string;
-    TotaldeRendimentos : string;
-    PrevidenciaPublica : string;
-    ImpostodeRenda : string;
-    DescontosDiversos : string;
-    RetençãoporTetoConstitucional : string;
-    TotaldeDescontos : string;
-    RendimentoLiquido : string;
-    Remuneraçãodoórgãodeorigem : string;
-    Diarias : string;
-    Orgao: string;
-    AnoReferencia: string;
-    AnoPublicacao: string
- }
+open NPOI.HSSF.UserModel
 
 let extractExcelLinksFromUrl url = 
 
@@ -52,17 +27,21 @@ let extractExcelLinksFromUrl url =
                     | x -> Some("http://www.cnj.jus.br/" + x)
                     )
 
-let downloadFile url = 
+let downloadExcelFileFromUrl url = 
     
+    printfn "Download Exel File From Url %s" url
+
     try
-        printfn "Download From Url %s" url
 
         let filename = ".data/download/" + Path.GetFileName url
-        let request = Http.AsyncRequestStream(url, timeout=1000*60*2)
-                      |> Async.RunSynchronously
+
+        if not (File.Exists filename) then
+
+            let request = Http.AsyncRequestStream(url, timeout=1000*60*2)
+                          |> Async.RunSynchronously
         
-        use outputFile = new FileStream(filename, FileMode.Create)
-        do request.ResponseStream.CopyTo(outputFile)
+            use outputFile = new FileStream(filename, FileMode.Create)
+            do request.ResponseStream.CopyTo(outputFile)
         
         Some(filename)
 
@@ -72,7 +51,7 @@ let downloadFile url =
     
 let formatCell (x:CellValue) =
 
-    if isNull x  then
+    if isNull x then
         ""
     else
         match x.CellType with
@@ -85,19 +64,18 @@ let formatCell (x:CellValue) =
 let validRow row = 
 
     let part = row |> Seq.filter (fun x-> x <> "***.***.***-**") 
-
-    let validateSeq x = Seq.exists (fun n -> n <> String.Empty) x
+    
     match row with
     | row when Seq.head row = "***.***.***-**" && 
                Seq.length row >= 17 &&
-               validateSeq part -> true
+               Seq.exists (fun n -> n <> String.Empty) part -> true
     | _ -> false
 
-let readFile filePath =
+let extractFileData filePath =
 
-    printfn "Map File %s" filePath
+    printfn "Extracting data from file %s" filePath
+
     let fs = new FileStream(filePath, FileMode.Open, FileAccess.Read)
-    
     let workbook = HSSFWorkbook fs
     let formulaEvaluator = workbook.GetCreationHelper().CreateFormulaEvaluator()
 
@@ -132,51 +110,27 @@ let readFile filePath =
         printfn "%s" e.Message
         None
 
-let saveFile data =
+let saveData (data: seq<seq<string>>) =
     
-    printfn "Print File"
+    printfn "Save data as CSV"
+    let outFile = new StreamWriter(".data/data.csv", false, Encoding.UTF8)
 
-    let outFile = new StreamWriter(".data/data.json", false, Encoding.UTF8)
-
+    let firstRow = "CPF,Nome,Cargo,Lotacao,Subsidio,DireitosPessoais,Indenizacoes,DireitosEventuais,TotaldeRendimentos,PrevidenciaPublica,ImpostodeRenda,DescontosDiversos,RetençãoporTetoConstitucional,TotaldeDescontos,RendimentoLiquido,Remuneraçãodoórgãodeorigem,Diarias,Orgao,AnoReferencia,AnoPublicacao"
     try
-        let jsonString = JsonConvert.SerializeObject(data, Formatting.Indented)
-        outFile.WriteLine(sprintf "%A," jsonString)
+        outFile.WriteLine(sprintf "%s," firstRow)
+        data 
+        |> Seq.iter(fun x -> 
+                        let row = System.String.Join(",", x)
+                        outFile.WriteLine(sprintf "%s," row)
+                    )
     with e ->
         printfn "%s" e.Message
     
     outFile.Close()
 
-let transformToJson (data: seq<seq<string>>) =
-    
-    seq { 
-        for item in data do
-            yield { 
-                CPF = Seq.item(0) item;
-                Nome = Seq.item(1) item;
-                Cargo = Seq.item(2) item;
-                Lotacao = Seq.item(3) item;
-                Subsidio = Seq.item(4) item;
-                DireitosPessoais = Seq.item(5) item;
-                Indenizacoes = Seq.item(6) item;
-                DireitosEventuais = Seq.item(7) item;
-                TotaldeRendimentos = Seq.item(8) item;
-                PrevidenciaPublica = Seq.item(9) item;
-                ImpostodeRenda = Seq.item(10) item;
-                DescontosDiversos = Seq.item(11) item;
-                RetençãoporTetoConstitucional = Seq.item(12) item;
-                TotaldeDescontos = Seq.item(13) item;
-                RendimentoLiquido = Seq.item(14) item;
-                Remuneraçãodoórgãodeorigem = Seq.item(15) item;
-                Diarias = Seq.item(16) item;
-                Orgao = Seq.item(17) item;
-                AnoReferencia = Seq.item(18) item;
-                AnoPublicacao = Seq.item(19) item;
-            }
-        }
-
-
 [<EntryPoint>]
 let main argv =
+
     printfn "Start!"
 
     if not (Directory.Exists ".data") then
@@ -185,11 +139,10 @@ let main argv =
         Directory.CreateDirectory ".data/download" |> ignore
 
     extractExcelLinksFromUrl  "http://www.cnj.jus.br/transparencia/remuneracao-dos-magistrados"
-    |> PSeq.choose downloadFile
-    |> PSeq.choose readFile
-    |> PSeq.map transformToJson
+    |> Seq.choose downloadExcelFileFromUrl
+    |> Seq.choose extractFileData
     |> Seq.concat
-    |> saveFile
+    |> saveData
     
     printfn "!End"
-    0 
+    0
